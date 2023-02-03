2c035501bdd568a0798b2d704ac067ffc1e83a02
AshishNitro <ashish.sharmar16@learner.manipal.edu>
2023-02-04 04:14:47 +0530

    loop

---
+++ loop.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.Scanner;
+public class loop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = " ";
+        while(name.isBlank()){
+            System.out.println("What is your name ");
+            name = sc.nextLine();
+
+        }
+        System.out.println("Hello /" +name);
+
+        
+    }
+}
